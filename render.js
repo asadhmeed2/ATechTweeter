d11cefb77acb7b0b9d770e7ff0f0343cd766d68d
newasad <asadhmeed2@gmail.com>
2023-11-28 16:26:24 +0200

    ..

--- render.js
+++ render.js
@@ -5,13 +5,16 @@ const Renderer = function(){
     const renderPosts = function(posts){
         postsView.empty();
         posts.forEach(function(post,idx){
-            postsView.append(`<div class="post" data-id="${post.id}" id="${post.id}">
+            postsView.append(`<div class="post" data-id="${post.id}" >
             ${post.text}
             </div>`)
 
-            const postElement = $(`#${post.id}`)
+            const postElement = $(`[data-id=${post.id}]`)
             post.comments.forEach(comment=>{
-                postElement.append(`<div class="comments" data-id="${comment.id}" id="${comment.id}">${comment.text}</div>`)
+                postElement.append(`<div class="comments" data-id="${comment.id}">
+                <span class="delete-comment">X</span>
+                <span >${comment.text}</span>
+                </div>`)
             })
         })
     }
