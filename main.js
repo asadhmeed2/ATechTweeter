9e207188792182be750a5efcce2fed9ca01c9505
newasad <asadhmeed2@gmail.com>
2023-11-28 20:43:25 +0200

    main.js / delete twit and add comment

--- main.js
+++ main.js
@@ -1,4 +1,40 @@
 const tweeter = TweeterModule()
 const renderer = Renderer()
 
-renderer.renderPosts(tweeter.getPosts())
+const input = $('#input');
+const posts = $('#posts');
+
+const renderPosts = ()=>{
+    renderer.renderPosts(tweeter.getPosts())
+}
+renderPosts();
+
+const post = function(){
+    const text = input.val();
+    
+    if(text === ''){
+        return 
+    }
+    
+    tweeter.addPost(text);
+    renderPosts();
+}
+
+posts.on('click','.delete', function(){
+    const id = $(this).closest('.post').data().id;
+    tweeter.removePost(id);
+    renderPosts();
+})
+
+posts.on('click','.commentBtn', function(){
+    const postId = $(this).closest('.post').data().id;
+    const text = $(this).closest('.post').find('.commentIpt').val();
+
+    if(!text){
+        return
+    }
+
+    tweeter.addComment(text,postId);
+    renderPosts();
+})
+
--- render.js
+++ render.js
@@ -1,14 +1,15 @@
-const postsView = $('#posts')
+
 
 const Renderer = function(){
-
+    const postsView = $('#posts');
+    
     const renderPosts = function(posts){
         postsView.empty();
         posts.forEach(function(post,idx){
             postsView.append(`<div class="post" data-id="${post.id}" >
-            ${post.text}
+                ${post.text}
             </div>`)
-
+            
             const postElement = $(`[data-id=${post.id}]`)
             post.comments.forEach(comment=>{
                 postElement.append(`<div class="comments" data-id="${comment.id}">
@@ -16,6 +17,15 @@ const Renderer = function(){
                 <span >${comment.text}</span>
                 </div>`)
             })
+
+            postElement.append(`<div>
+                <input type="text" placeholder="Got Something to say?" class="commentIpt"/> 
+                <button class="commentBtn">Comment</button>
+            </div>`)
+            postElement.append(`<div class="delete">delete</div>`)
+
+
+
         })
     }
 
